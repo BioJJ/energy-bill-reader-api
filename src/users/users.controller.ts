8d162b1e84784b8adf14b2e15b4707c8b7f2260e
Jefferson Coelho <bio.jeffcoelho@gmail.com>
2023-08-06 06:16:17 -0400

    refactor: ♻️ make the user registration endpoint public

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,11 +11,13 @@ import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './entities/user.entity'
+import { IsPublic } from 'src/auth/decorators/is-public.decorator'
 
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
+	@IsPublic()
 	@Post()
 	async create(@Body() createUserDto: CreateUserDto): Promise<User> {
 		return await this.usersService.create(createUserDto)
